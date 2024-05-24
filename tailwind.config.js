75d654219d5c7355726f27f8c6c5d505ab738ed9
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-05-25 01:37:00 +0600

    new project setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        fontRokkitt:['Rokkitt', 'serif'],
+        fontMulish :['Mulish', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
