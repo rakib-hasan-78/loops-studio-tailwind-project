8de7fefca036b17265d5770bae3a4798646bd168
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-05-26 18:18:50 +0600

    footer done

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import './src/css/style.css'
 
+
+
+const dynamicContent =(id, text) =>{
+    document.getElementById(id).innerHTML=text
+}
+dynamicContent('copy', `&copy; ${new Date().getFullYear()} Loopstudios. All Rights Reserved`);
+
+dynamicContent('credit', `Developed by Md Rakibul Hasan.`);
